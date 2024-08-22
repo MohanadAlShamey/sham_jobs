13e1eb2e0db50d4d4c58a39d64e73863362e3e1b
ADNAN_DEV <mh.shamey@gmail.com>
2024-08-22 16:20:42 +0300

    add avtive & sort ask

--- app/Filament/Resources/JobResource.php
+++ app/Filament/Resources/JobResource.php
@@ -50,7 +50,7 @@ class JobResource extends Resource
                 Tables\Actions\EditAction::make(),
                 Tables\Actions\ViewAction::make(),
                 Tables\Actions\DeleteAction::make(),
-                Tables\Actions\Action::make('download')->url(fn($record)=>route('download-resumes',$record->id),true)->label('تحميل جميع المرفقات'),
+                Tables\Actions\Action::make('download')->url(fn($record)=>route('download-resumes',$record->id),true)->label('تحميل جميع المرفقات')->button()->success(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
