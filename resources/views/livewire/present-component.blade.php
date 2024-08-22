cae4b46f8d508e8d8d73e3917e5a60663a5b68bf
ADNAN_DEV <mh.shamey@gmail.com>
2024-08-22 11:30:13 +0300

    add api

--- resources/views/livewire/present-component.blade.php
+++ resources/views/livewire/present-component.blade.php
@@ -6,21 +6,21 @@
                 @csrf
                 <input type="hidden" name="job_id" value="{{$job->id}}">
               <div class="form-group my-4">
-                    <label for="" class="required">البريد الإلكتروني : </label>
+                    <label for="" class="required h6">البريد الإلكتروني : </label>
                     <input type="email" required class="form-control @error('email') is-invalid @endError" name='email'
                            value="{{old('email')}}">
                     @error('email') <span class="text-danger">{{$message}}</span> @endError
                 </div>
 
                 <div class="form-group my-4">
-                    <label for="" class="required">المسمى الوظيفي : </label>
+                    <label for="" class="required h6">المسمى الوظيفي : </label>
                     <input type="text" required class="form-control @error('job_name') is-invalid @endError"
                            name='job_name' value="{{old('job_name')}}">
                     @error('job_name') <span class="text-danger">{{$message}}</span> @endError
                 </div>
 
                 <div class="form-group my-4">
-                    <label for="" class="required">الاسم الأول كما هو وارد في الهوية الشخصية أو جواز السفر
+                    <label for="" class="required h6">الاسم الأول كما هو وارد في الهوية الشخصية أو جواز السفر
                         : </label>
                     <input type="text" required class="form-control  @error('first_name') is-invalid @endError"
                            name='first_name' value="{{old('first_name')}}">
@@ -28,7 +28,7 @@
                 </div>
 
                 <div class="form-group my-4">
-                    <label for="" class="required">اسم الأب كما هو وارد في بطاقة الهوية الشخصية أو جواز السفر
+                    <label for="" class="required h6">اسم الأب كما هو وارد في بطاقة الهوية الشخصية أو جواز السفر
                         : </label>
                     <input type="text" required class="form-control  @error('father_name') is-invalid @endError"
                            name='father_name' value="{{old('father_name')}}">
@@ -36,7 +36,7 @@
                 </div>
 
                 <div class="form-group my-4">
-                    <label for="" class="required">الكنية كما هي واردة في بطاقة الهوية الشخصية أو جواز السفر
+                    <label for="" class="required h6">الكنية كما هي واردة في بطاقة الهوية الشخصية أو جواز السفر
                         : </label>
                     <input type="text" required class="form-control  @error('last_name') is-invalid @endError"
                            name='last_name' value="{{old('last_name')}}">
@@ -44,7 +44,7 @@
                 </div>
 
                 <div class="form-group my-4">
-                    <label for="" class="required">تاريخ الميلاد
+                    <label for="" class="required h6">تاريخ الميلاد
                         : </label>
                     <input type="date" required class="form-control  @error('birth_date') is-invalid @endError"
                            name='birth_date' value="{{old('birth_date')}}">
@@ -56,7 +56,7 @@
                     @switch($ask->type)
                         @case(\App\Enums\AskTypeEnum::EMAIL->value)
                         <div class="form-group my-4">
-                            <label for="" @if($ask->required) class="required" @endif>{{$ask->title}} : </label>
+                            <label for="" @if($ask->required) class="required h6" @endif>{{$ask->title}} : </label>
                             <input type="email" class="form-control @error($ask->id) is-invalid @endError"
                                    value="{{old($key)}}" @if($ask->required) required="required"
                                    @endif   name='{{$ask->id}}'>
@@ -66,7 +66,7 @@
 
                         @case(\App\Enums\AskTypeEnum::TEXT->value)
                         <div class="form-group my-4">
-                            <label for="" @if($ask->required) class="required" @endif>{{$ask->title}} : </label>
+                            <label for="" @if($ask->required) class="required h6" @endif>{{$ask->title}} : </label>
                             <input type="text" class="form-control @error($ask->id) is-invalid @endError"
                                    value="{{old($ask->id)}}" @if($ask->required) required="required"
                                    @endif  name='{{$ask->id}}'>
@@ -76,7 +76,7 @@
 
                         @case(\App\Enums\AskTypeEnum::NUMBER->value)
                         <div class="form-group my-4">
-                            <label for="" @if($ask->required) class="required" @endif>{{$ask->title}} : </label>
+                            <label for="" @if($ask->required) class="required h6" @endif>{{$ask->title}} : </label>
                             <input type="number" class="form-control @error($ask->id) is-invalid @endError"
                                    value="{{old($ask->id)}}" @if($ask->required) required="required"
                                    @endif   name='{{$ask->id}}'>
@@ -86,7 +86,7 @@
 
                         @case(\App\Enums\AskTypeEnum::FILE->value)
                         <div class="form-group my-4">
-                            <label for="" @if($ask->required) class="required" @endif>{{$ask->title}} : </label>
+                            <label for="" @if($ask->required) class="required h6" @endif>{{$ask->title}} : </label>
                             <input type="file" class="form-control @error($ask->id) is-invalid @endError"
                                    value="{{old($ask->id)}}" @if($ask->required) required="required"
                                    @endif  name='{{$ask->id}}'>
@@ -95,7 +95,7 @@
                         @break
                         @case(\App\Enums\AskTypeEnum::DATE->value)
                         <div class="form-group my-4">
-                            <label for="" @if($ask->required) class="required" @endif>{{$ask->title}} : </label>
+                            <label for="" @if($ask->required) class="required h6" @endif>{{$ask->title}} : </label>
                             <input type="date" class="form-control @error($ask->id) is-invalid @endError"
                                    value="{{old($ask->id)}}" @if($ask->required) required="required"
                                    @endif  name='{{$ask->id}}'>
@@ -103,14 +103,14 @@
                         </div>
                         @break
                         @case(\App\Enums\AskTypeEnum::RADIO->value)
-                        <label for="" @if($ask->required) class="required" @endif>{{$ask->title}}</label>
+                        <label for="" @if($ask->required) class="required h6" @endif>{{$ask->title}}</label>
                         @foreach($ask->options as $key=>$option)
                             <div class="form-check">
 
-                                <label class="form-check-label" for="flexRadioDefault{{$key}}" class="d-inline-block "
+                                <label  for="flexRadioDefault{{$key}}" class="form-check-label d-inline-block "
                                        dir="ltr">
 
-                                    <span class="d-inline-block"> {{$option['option']}}</span>
+                                    <span class="d-inline-block small"> {{$option['option']}}</span>
                                     <input class="form-check-input" @if($loop->first) checked @endif type="radio"
                                            value="{{$option['option']}}"
                                            name="{{$ask->id}}" id="flexRadioDefault{{$key}}">
@@ -125,12 +125,12 @@
 
                         @case(\App\Enums\AskTypeEnum::CHECKBOX->value)
                         <label for="" @if($ask->required) class="required"
-                               @endif  @if($ask->required) required="required" @endif >{{$ask->title}}</label>
+                               @endif  @if($ask->required) required="required h6" @endif >{{$ask->title}}</label>
                         @foreach($ask->options as $key=>$option)
                             <div class="form-check">
 
                                 <label class="form-check-label" for="flexRadioDefault{{$key}}">
-                                    <span> {{$option['option']}}</span>
+                                    <span class="small"> {{$option['option']}}</span>
 
                                     <input @if($loop->first) checked @endif class="form-check-input" type="checkbox"
                                            value="{{$option['option']}}"
