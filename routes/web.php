e0100d63a2d30495dd4f23dd4e586b63d0b9ceeb
sham <albasha.sq@gmail.com>
2025-04-10 10:56:46 +0300

    add export

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Maatwebsite\Excel\Excel;
+use Maatwebsite\Excel\Facades\Excel;
 
 /*
 |--------------------------------------------------------------------------
